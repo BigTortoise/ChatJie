186d220923fd536cc5c7fb04e804fef63f22f479
BigTortoise <302899093@qq.com>
2016-05-03 19:25:20 +0800

    初始化项目

---
+++ ChatJie/ChatJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatJie
+//
+//  Created by 王 on 16/5/3.
+//  Copyright © 2016年 renyijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatJie/ChatJie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatJie
+//
+//  Created by 王 on 16/5/3.
+//  Copyright © 2016年 renyijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
